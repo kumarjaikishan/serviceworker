0f42af25cc8359a5618947f07a0ebdd1425701b6
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-31 23:32:13 +0530

    xvs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 const { Worker } = require('bullmq');
 const IORedis = require('ioredis');
 const sendmail = require('./sendemail');
