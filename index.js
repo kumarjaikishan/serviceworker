af75210ab58a42b825d60112eaf696bad26e98fa
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-31 22:32:51 +0530

    jjbjjk

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6000;
 const { Worker } = require('bullmq');
 const IORedis = require('ioredis');
 const sendmail = require('./sendemail');
 const push_notification = require('./push_notification');
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+      msg: "Welcome to the Service Worker"
+    })
+  })
 
 const sendEmailhelper = async (n) => {
     return new Promise((res, rej) => setTimeout(() => res(), n * 1000))
@@ -17,14 +26,15 @@ async function pushNotification(job) {
     await push_notification(registeredToken, mes);
     await sendEmailhelper(1);
 }
-
+const sharedConnection = new IORedis(process.env.REDIS_URIfulle, {
+    maxRetriesPerRequest: null,
+    maxmemoryPolicy: 'noeviction', // Set the eviction policy to "noeviction"
+});
 
 const worker = new Worker('battlefiesta_queue',
     sendEmail,
     {
-        connection: new IORedis(process.env.REDIS_URIfulle, {
-            maxRetriesPerRequest: null,
-        }),
+        connection: sharedConnection,
     });
 
 worker.on('completed', job => {
@@ -37,9 +47,7 @@ worker.on('failed', (job, err) => {
 const worker2 = new Worker('firebase_push_notification',
     pushNotification,
     {
-        connection: new IORedis(process.env.REDIS_URIfulle, {
-            maxRetriesPerRequest: null,
-        }),
+        connection: sharedConnection,
     });
 
 worker2.on('completed', job => {
@@ -49,3 +57,7 @@ worker2.on('completed', job => {
 worker2.on('failed', (job, err) => {
     console.log(`${job.id} has failed - ${err}`);
 })
+
+app.listen(port, () => {
+    console.log(`server listening at ${port}`);
+  })
